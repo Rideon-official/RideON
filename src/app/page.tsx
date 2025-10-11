474b12bea3e408f5e2bad63461d4f90c5c5c0c42
Rideon-official <rideon_official@naver.com>
2025-10-11 19:02:16 +0900

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
 import ConvergeToText from "../components/ConvergeToText";
-import Image from "next/image";
 import HeroParticles from "../components/HeroParticles";
 
 export default function Home() {
