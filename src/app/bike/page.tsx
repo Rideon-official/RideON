cafa057d4b9fbd3deea9d12c2707ac3d8fc31fdf
Rideon-official <rideon_official@naver.com>
2025-10-15 02:53:13 +0900

    page.tsx 업데이트

--- src/app/bike/page.tsx
+++ src/app/bike/page.tsx
@@ -1,19 +1,17 @@
 // src/app/bike/page.tsx
 import BikeIntro from "../../components/bike/BikeIntro";
 import BikeQuickApply from "../../components/bike/BikeQuickApply";
-import ServiceCenters from "../../components/center/ServiceCenters";
+import BikeCenters from "../../components/bike/BikeCenters";
 
 export default function BikePage() {
   return (
     <main className="bg-black text-white">
       {/* 1) 렌트/리스 소개 */}
       <BikeIntro />
-
-      {/* 2) 렌트/리스 신청 (가벼운 CTA) */}
+      {/* 2) 렌트/리스 신청(가벼운 CTA) */}
       <BikeQuickApply />
-
-      {/* 3) 센터 위치 및 안내 (하단) */}
-      <ServiceCenters />
+      {/* 3) 센터 위치 및 안내(하단) */}
+      <BikeCenters />
     </main>
   );
 }
